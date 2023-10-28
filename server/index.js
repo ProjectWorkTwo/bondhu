ddde57f17cb0e4f2be4af741b6cf12ea44d38327
developerHub01 <developerhubytchannel@gmail.com>
2023-10-29 04:15:52 +0600

    server routes added

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-
+const cors = require("cors");
+const port = process.env.PORT || 8001;
+const authRouter = require("./routers/auth");
+const profileRoute = require("./routers/profile");
+const homeRoute = require("./routers/home");
+const groupRoute = require("./routers/group");
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(authRouter);
+app.use(homeRoute);
+app.use(profileRoute);
+app.use(groupRoute);
+
+app.listen(port, () => {
+  console.log(`server is running at http://localhost:${port}`);
+});
